764be197a289e6077a3357599c6d7aeff3e3b89f
Vinicius-Chagas <vinicandido00@gmail.com>
2024-03-18 21:15:36 -0300

    corrigindo conflito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
-import { CardComponent } from './components/card/card.component';
-import { SliderComponent } from './components/slider/slider.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    CardComponent,
-    SliderComponent,
-    SidebarComponent
-=======
 @NgModule({
   declarations: [
     AppComponent
->>>>>>> c3b400dad7f492b3c9ec882da98cae893a6292d5
   ],
   imports: [
     BrowserModule,
