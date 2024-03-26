ceb162de5a911c12736b99991af173bf1f50df22
Renan Vizoto Ferreira <108243621+Renan-Vizoto@users.noreply.github.com>
2024-03-25 21:51:51 -0300

    feat: primeiro commit da estrutura da tela de login e de registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TelaLoginComponent } from "./pages/login/tela-login.component";
+import { RegisterComponent } from "./pages/register/register.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: TelaLoginComponent }
+  { path: 'login', component: TelaLoginComponent },
+  { path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
